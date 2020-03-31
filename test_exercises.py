084708142057713912d397842202409a4515df92
Kevin Lacker <lacker@gmail.com>
2020-03-31 10:49:00 -0700

    Testing

---
+++ test_exercises.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import unittest
+
+from pyquil.api import local_forest_runtime
+
+from exercises import *
+
+
+class TestExercises(unittest.TestCase):
+    def test_octahedral_die(self):
+        outcomes = [throw_octahedral_die() for _ in range(80)]
+        for outcome in outcomes:
+            self.assertLessThan(outcome, 9)
+            self.assertGreaterThan(outcome, 0)
+
+
+if __name__ == "__main__":
+    with local_forest_runtime():
+        unittest.main()
